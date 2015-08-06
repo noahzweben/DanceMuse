8e5982b5628fd1c15401e73a3294c10a6fd895e8
Noah Zweben <noahzweben@gmail.com>
2015-08-05 21:13:34 -0400

    Delete fx.py

--- fx.py
+++
@@ -1,8 +0,0 @@
-# A module of Automated effects #
-from random import *
-from Timer import *
-
-def fx_random(minval, maxval, function, interval):
-	"""randomly automates any function that takes a number as input)"""
-	timer = Timer(interval,function,[mapValue(random(),0,1,minval,maxval)])
-	return timer 
