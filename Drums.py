ff2d2e574cab47ad8fcc1c237a413bb140a56dc3
Noah Zweben <noahzweben@gmail.com>
2015-08-13 09:05:14 -0400

    made fast tempo cool electro sound

--- Drums.py
+++ Drums.py
@@ -1,11 +1,12 @@
 from music import *
-
+import time
 
 class Drums:
 	CHANNEL = 9
 	def __init__(self):
 		self.beatNumber = 0
 		self.beat = None
+		self.isRain = False
 
 	
 	def startBeat(self,beat,tempo):
@@ -31,6 +32,25 @@ class Drums:
 			Play.noteOn(BDR,127,self.CHANNEL)
 		self.beatNumber+=1
 		self.beatNumber= self.beatNumber%4
+
+	def electroBeat(self):
+		if self.beatNumber == 0:
+			Play.noteOn(40,127,self.CHANNEL)
+		else:
+			Play.noteOn(39,127,self.CHANNEL)
+		self.beatNumber+=1
+		self.beatNumber= self.beatNumber%4
+
+
+	def raindrops(self,tempo,inst):
+		self.isRain = True
+		tempo = tempo/1000.0
+
+		for i in range(16):
+			Play.noteOn(122,90,15)
+			time.sleep(.05)
+
+		self.isRain = False
 
 
 
--- main.py
+++ main.py
@@ -10,15 +10,14 @@ import time
 bassChords = Chord()
 drums = Drums()
 melody_high = Melody(channel=11,octaveRange=[C4,B5],baseNote=C4,volume=100, instrument = 41)
-melody_low = Melody(channel=10,octaveRange=[C1,B2],baseNote=C2,volume=100, instrument = 41)
-melody_low.standardDuration([.25])
+melody_low = Melody(channel=10,octaveRange=[C1,B2],baseNote=C2,volume=127, instrument = 97)
 phoneIn = PhoneConnect()
 
+
+#Waits for OSC connection to beign loading data 
 print "Loading"
-while (phoneIn.xAccel is None) or (phoneIn.pitch is None):
+while (phoneIn.xAccel is None) or (phoneIn.pitch is None): #Waits for OSC connection to beign loading data 
 	pass
-
-
 print "Begin Dancing"
 
 for i in range(3,0,-1):
@@ -27,14 +26,16 @@ for i in range(3,0,-1):
 
 #Start Dancing
 tempo = phoneIn.getBeat() #captures tempo
+print tempo/1000.0
 #
 if tempo<2000:
-	bassChords = Chord(octaveRange=[C3,B4],instrument = 27)
-	melody_high.setInstrument(27)
-	drums.startBeat(drums.classicBeat,tempo)
+	bassChords = Chord(octaveRange=[C3,B4],instrument = 30,volume=80)
+	melody_high.setInstrument(30)
+	drums.startBeat(drums.electroBeat,tempo)
 
 bassChords.autoProgressChord(tempo)
 melody_high.playMelody(bassChords,tempo)
+melody_low.playMelody(bassChords,tempo*2)
 
 
 
